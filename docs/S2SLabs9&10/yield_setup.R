208ce6ac770aad6f71cce877c08278a8422cad7a
Glasgow-Stats-L1-L2 <111056800+Glasgow-Stats-L1-L2@users.noreply.github.com>
2023-11-21 12:00:57 +0000

    Update 9&10

---
+++ docs/S2SLabs9&10/yield_setup.R
@@ -0,0 +1,22 @@
+#farmer has used fertiliser or not and measured potato yield and lettuce yield
+
+set.seed(1989)
+
+lettuce_fert <- rnorm(n = 13, mean = 24, sd = 3)
+lettuce_nofert <- rnorm(n = 8, mean = 18, sd = 1)
+
+pot_fert <- rnorm(n = 13, mean = 39, sd = 4)
+pot_nofert <- rnorm(n = 8, mean = 35, sd = 4)
+
+supermarket_ind <- sample(1:21, 15)
+supermarket <- numeric(21)
+supermarket[supermarket_ind] <- 1
+
+yields <- data.frame(fertiliser = rep(c("Used", "Not used"), times = c(13, 8)),
+                     potato = c(pot_fert, pot_nofert),
+                     lettuce = c(lettuce_fert, lettuce_nofert),
+                     supermarket = supermarket)
+yields <- yields[sample(nrow(yields)), ]
+row.names(yields) <- 1:nrow(yields)
+
+write.csv(x = yields, file = "Data/yields.csv", row.names = FALSE)
